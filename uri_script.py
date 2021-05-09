1742d429066651eea2ab1162e65ae5be798f52a7
William Furtado <willfurtado@berkeley.edu>
2021-05-09 09:54:30 -0700

    Writing CSVs for features in current playlists

--- uri_script.py
+++ uri_script.py
@@ -4,13 +4,9 @@ import json
 import spotipy
 
 from spotipy.oauth2 import SpotifyOAuth
+from secrets import *
 
 plt.style.use('ggplot')
-
-CLIENT_USERNAME = "willfurtado"
-SPOTIPY_CLIENT_ID = "0c58b8f377294e1393b6ff20d1db34fc"
-SPOTIPY_CLIENT_SECRET = "12fb3865a39343aba75ec4b118f6adf9"
-SPOTIPY_REDIRECT_URI = "https://localhost:8888"
 
 scope = "user-library-read"
 
